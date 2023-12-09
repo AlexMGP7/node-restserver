b8db0662a010067258a0aff5dc41fff064540641
AlexMGP7 <agonzalezpastena7t@gmail.com>
2023-12-09 10:12:04 -0400

    verison-final 4

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
     const { correo, password } = req.body;
@@ -33,7 +34,7 @@ const login = async (req, res = response) => {
             },
             token
         });
-        
+
 
     } catch (error) {
         console.error('Error en el sistema de login:', error);
@@ -41,6 +42,56 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { nombre, img, correo } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true,
+                rol: 'USER_ROLE'
+
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        console.log(error); // Agrega esta línea
+        res.status(400).json({
+            ok: false,
+            msg: 'El Token no se pudo verificar'
+        })
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 };
